160e06f0151ffaf0907b2978a2d3fca28e284b06
TheSteeNos <davidsteens2001@gmail.com>
2024-09-06 21:21:09 +0200

    Polishes stats and added fault in guessing words

--- game.mjs
+++ game.mjs
@@ -13,23 +13,27 @@ import { ANSI } from './ansi.mjs';
 import { HANGMAN_UI } from './graphics.mjs';
 
 let willPlayerContinue = true;
-let wrongGuesses = [];
-let totalWrongGuesses = [];
+let totalWrongGuesses = 0;
+let totalCompletedPuzzles = 0;
+let totalRoundsPlayed = 0;
 
 const PLAYER_PROMPTS_AND_INFO = {
 WIN: "Congratulations! You got the correct word!",
 GUESS_PROMPT: "Guess a character or the word: ",
 GAME_OVER: "Game Over!",
 ANSWER_REVEAL: "The answer was: ",
-CONTINUE_PROMPT: "Want to play again? Enter 1 for yes, or 2 for no: ",
+CONTINUE_PROMPT: "Want to play again? Enter yes or no: ",
 THANK_PLAYER: "Thank you for playing!",
 STAT_INFO: "Statistics: ",
-WRONG_GUESS_AMOUNT: "Number of wrong guesses: ",
-PLAYER_ACCEPT: "1",
-PLAYER_DENY: "2",
+WRONG_GUESS_AMOUNT: "Total wrong guesses: ",
+COMPLETED_PUZZLES: "Total completed puzzles: ",
+ROUNDS_PLAYED: "Total rounds played: ",
+PLAYER_ACCEPT: "yes",
+PLAYER_DENY: "no",
+EXIT_PROMPT: "Enter any key to exit the program: "
 };
 
-const CHAR_AND_WORDLIST = {
+const CHAR = {
 SPACE: " ",
 EMPTY: "",
 UNDERBRACKET: "_",
@@ -38,10 +42,11 @@ UNDERBRACKET: "_",
 while (willPlayerContinue == true) {
 
     let words = ["apple", "mountain", "blue", "whisper", "chair", "lamp", "rain", "keyboard", "sunshine", "tiger", "basket", "cloud", "ocean", "planet", "book", "bicycle", "river", "bridge", "garden", "camera", "pillow", "blanket", "window", "forest", "elephant", "car", "guitar", "flame", "shadow", "ice", "paper", "rose", "butterfly", "engine", "school", "village", "coffee", "feather", "desert", "mirror", "snow", "pencil", "castle", "door", "diamond", "rocket", "piano", "moon", "sail", "train", "volcano", "cup", "chocolate", "silk", "puzzle", "ghost", "pocket", "street", "flute", "honey", "sand", "library", "mountain", "parrot", "hat", "fire", "star", "candle", "notebook", "key", "watch", "sign", "shoe", "pyramid", "banana", "pearl", "ship", "tree", "sun", "cliff", "pear", "dragon", "waterfall", "clock", "glove", "wind", "egg", "lemon", "mirror", "river", "cactus", "whale", "match", "bell", "forest", "crown", "ribbon", "motor", "violet", "umbrella", "kite", "rainbow", "scarf", "wood", "breeze", "cloud", "wheel", "mountain", "light", "leaf", "spoon", "bench", "rabbit", "piano", "grape", "teapot", "hill", "button", "storm", "shell", "sunset", "bucket", "spark", "owl", "thread", "snowflake", "bridge", "suitcase", "keyhole", "radio", "lamp", "door", "apple", "paper", "cotton", "salt", "fountain", "cup", "jungle", "straw", "castle", "seed", "plank", "fish", "orange", "coin", "thread", "shark", "gem", "tooth", "motor", "chalk", "horn", "bush", "beard", "ice", "tunnel", "kite", "pebble", "butter", "feather", "firefly", "stone", "puddle", "crane", "rocket", "bone", "bucket", "shadow", "yarn", "frost", "garden", "cane", "glue", "globe", "hedge", "ship", "helmet", "chalk", "rope", "mask", "fan", "zipper", "breeze", "shovel", "vest", "blanket", "tent", "stick", "fence", "towel", "leash", "jar", "kite", "toy", "shell", "moss", "arrow", "brick", "cloud", "cushion"];
-    let wordDisplay = CHAR_AND_WORDLIST.EMPTY;
+    let wordDisplay = CHAR.EMPTY;
     let correctWord = words[getRandomNumber(words.length)];
     const numberOfCharInWord = correctWord.length;
-    let guessedWord = CHAR_AND_WORDLIST.EMPTY.padStart(correctWord.length, CHAR_AND_WORDLIST.UNDERBRACKET);
+    let guessedWord = CHAR.EMPTY.padStart(correctWord.length, CHAR.UNDERBRACKET);
+    let wrongGuesses = [];
     let isGameOver = false;
     let wasGuessCorrect = false;
 
@@ -57,10 +62,11 @@ while (willPlayerContinue == true) {
         if (answer == correctWord) {
             isGameOver = true;
             wasGuessCorrect = true;
-        } else if (ifPlayerGuessedLetter(answer)) {
+        
+        } else if (ifPlayerGuessed(answer)) {
 
             let org = guessedWord;
-            guessedWord = CHAR_AND_WORDLIST.EMPTY;
+            guessedWord = CHAR.EMPTY;
 
             let isCorrect = false;
             for (let i = 0; i < correctWord.length; i++) {
@@ -74,9 +80,11 @@ while (willPlayerContinue == true) {
 
             if (isCorrect == false) {
                 wrongGuesses.push(answer);
+                totalWrongGuesses++;
             } else if (guessedWord == correctWord) {
                 isGameOver = true;
                 wasGuessCorrect = true;
+                totalCompletedPuzzles++;
             }
         }
 
@@ -92,7 +100,7 @@ while (willPlayerContinue == true) {
     console.log(HANGMAN_UI[wrongGuesses.length]);
 
     if (wasGuessCorrect) {
-        console.log(ANSI.COLOR.YELLOW + PLAYER_PROMPTS_AND_INFO.WIN);
+        console.log(ANSI.COLOR.YELLOW + PLAYER_PROMPTS_AND_INFO.WIN + ANSI.RESET);
     }
 
     if (wrongGuesses.length == HANGMAN_UI.length - 1) {
@@ -100,10 +108,8 @@ while (willPlayerContinue == true) {
         console.log(ANSI.COLOR.RED + PLAYER_PROMPTS_AND_INFO.GAME_OVER + ANSI.RESET);
     }
 
-    totalWrongGuesses.push(wrongGuesses);
-    wrongGuesses = [];
-
-    const answer = (await askQuestion(PLAYER_PROMPTS_AND_INFO.CONTINUE_PROMPT));
+    const answer = (await askQuestion(PLAYER_PROMPTS_AND_INFO.CONTINUE_PROMPT)).toLowerCase();
+    totalRoundsPlayed++;
 
     if (answer == PLAYER_PROMPTS_AND_INFO.PLAYER_ACCEPT) {
         willPlayerContinue = true;
@@ -119,8 +125,8 @@ while (willPlayerContinue == true) {
     
     }
     
-    function ifPlayerGuessedLetter(answer) {
-        return answer.length == 1;
+    function ifPlayerGuessed(answer) {
+        return answer.length;
     }
     
     function listOfWrongGuesses(list, color) {
@@ -153,8 +159,14 @@ while (willPlayerContinue == true) {
 
 console.log(ANSI.CLEAR_SCREEN);
 console.log(PLAYER_PROMPTS_AND_INFO.THANK_PLAYER)
-console.log("");
+console.log(CHAR.EMPTY);
 console.log(PLAYER_PROMPTS_AND_INFO.STAT_INFO);
-console.log(PLAYER_PROMPTS_AND_INFO.WRONG_GUESS_AMOUNT + ANSI.COLOR.RED + totalWrongGuesses.length + ANSI.RESET);
-
-
+console.log(PLAYER_PROMPTS_AND_INFO.ROUNDS_PLAYED + ANSI.COLOR.YELLOW + totalRoundsPlayed + ANSI.RESET);
+console.log(PLAYER_PROMPTS_AND_INFO.COMPLETED_PUZZLES + ANSI.COLOR.GREEN + totalCompletedPuzzles + ANSI.RESET);
+console.log(PLAYER_PROMPTS_AND_INFO.WRONG_GUESS_AMOUNT + ANSI.COLOR.RED + totalWrongGuesses + ANSI.RESET);
+console.log(CHAR.EMPTY);
+const answer = (await askQuestion(PLAYER_PROMPTS_AND_INFO.EXIT_PROMPT));
+if (answer) {
+    console.log(ANSI.CLEAR_SCREEN);
+    process.exit();
+}
